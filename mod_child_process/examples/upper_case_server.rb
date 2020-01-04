1f85948f8681939c6ea59c72a5433c874892c850
vain0x <vainzerox@gmail.com>
2020-01-04 20:32:18 +0900

    docs: Ruby スクリプトをサーバーとして動作させるサンプルを追加

---
+++ mod_child_process/examples/upper_case_server.rb
@@ -0,0 +1,22 @@
+# LICENSE: Unlicensed (PUBLIC DOMAIN)
+# サーバーサイド
+
+while true do
+    # 標準入力から文字列を受け取る。
+    s = gets
+
+    # gets の結果には改行文字が含まれるので、chomp で改行文字を取り除く。
+    s = s.chomp
+
+    # 標準入力が終了したら、あるいは「EXIT」が来たらサーバーも停止する。
+    if s == nil || s == "EXIT" then
+        break
+    end
+
+    # 文字列をすべて大文字にする。
+    s = s.upcase
+
+    # 文字列を標準出力に出力する。
+    puts s
+    STDOUT.flush
+end
